329826a28fe069a113b0d81a0abea283316c3479
Ryan Brideau <ryan.brideau@gmail.com>
2014-08-24 12:18:15 -0300

    Commented out testing code

--- cachematrix.R
+++ cachematrix.R
@@ -34,20 +34,19 @@ cacheSolve <- function(x, ...) {
     m
 }
 
-width <- 200
-matrix <- matrix(floor(rexp(width^2, rate=.1)), ncol=width, nrow=width)
-microbenchmark(inverted <- solve(matrix)) # 17.78 ms
-
-small.matrix <- matrix(c(4, 3, 3, 2), 2, 2)
-microbenchmark(small.matrix.inverse <- solve(small.matrix)) # 18.32 us
-
-# Using cache
-# Create the vector object
-special.object <- makeCacheMatrix()
-special.object$set(matrix)
-special.object$get()
-microbenchmark(cacheSolve(special.object)) # 115 us
-special.object$getinverse()
+# Example code to test functionality and speed
+#
+# width <- 200
+# matrix <- matrix(floor(rexp(width^2, rate=.1)), ncol=width, nrow=width)
+# microbenchmark(inverted <- solve(matrix)) # 17.78 milliseconds
+# 
+# # Using cache
+# # Create the vector object
+# special.object <- makeCacheMatrix()
+# special.object$set(matrix)
+# special.object$get()
+# microbenchmark(cacheSolve(special.object)) # 115 microseconds
+# special.object$getinverse()
 
 
 
