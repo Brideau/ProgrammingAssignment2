162c857234ca84f99cc9ee2140dda219e81484f7
Ryan Brideau <ryan.brideau@gmail.com>
2014-08-24 12:16:52 -0300

    Solution commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions allows you to compute the inverse of a matrix
+## and cache the result. If the same inverse is calculated again in the future
+## the cached result will be provided instead of re-computing it.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## `makeCacheMatrix` above. If the inverse has already been calculated (and
+## the matrix has not changed), then `cacheSolve` retrieves the 
+## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("Getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
+
+width <- 200
+matrix <- matrix(floor(rexp(width^2, rate=.1)), ncol=width, nrow=width)
+microbenchmark(inverted <- solve(matrix)) # 17.78 ms
+
+small.matrix <- matrix(c(4, 3, 3, 2), 2, 2)
+microbenchmark(small.matrix.inverse <- solve(small.matrix)) # 18.32 us
+
+# Using cache
+# Create the vector object
+special.object <- makeCacheMatrix()
+special.object$set(matrix)
+special.object$get()
+microbenchmark(cacheSolve(special.object)) # 115 us
+special.object$getinverse()
+
+
+
+
+
+
